92d6d6c645af91f3803a992616386af82da9bbfd
enzo418 <c.enzoaguirre@gmail.com>
2023-03-27 16:07:05 -0300

    publish event domain from db context

--- FisherTournament.Infrastracture/Persistence/TournamentFisherDbContext.cs
+++ FisherTournament.Infrastracture/Persistence/TournamentFisherDbContext.cs
@@ -4,6 +4,8 @@ using FisherTournament.Domain.CompetitionAggregate.Entities;
 using FisherTournament.Domain.FisherAggregate;
 using FisherTournament.Domain.TournamentAggregate;
 using FisherTournament.Domain.UserAggregate;
+using FisherTournament.Infrastracture.Mediator;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace FisherTournament.Infrastracture.Persistence;
@@ -15,16 +17,19 @@ public class TournamentFisherDbContext : DbContext, ITournamentFisherDbContext
     public DbSet<User> Users { get; set; } = null!;
     public DbSet<Fisher> Fishers { get; set; } = null!;
 
+    private readonly IMediator _mediator = null!;
+
     public TournamentFisherDbContext(
-        DbContextOptions<TournamentFisherDbContext> options)
+        DbContextOptions<TournamentFisherDbContext> options,
+        IMediator mediator)
         : base(options)
     {
-
+        _mediator = mediator;
     }
 
-    public TournamentFisherDbContext()
+    public TournamentFisherDbContext(IMediator mediator)
     {
-
+        _mediator = mediator;
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -36,5 +41,16 @@ public class TournamentFisherDbContext : DbContext, ITournamentFisherDbContext
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(TournamentFisherDbContext).Assembly);
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        await _mediator.DispatchDomainEventsBeforeSaveAsync(this, cancellationToken);
+
+        int changes = await base.SaveChangesAsync(cancellationToken);
+
+        await _mediator.DispatchDomainEventsAfterSaveAsync(this, cancellationToken);
+
+        return changes;
     }
 }
