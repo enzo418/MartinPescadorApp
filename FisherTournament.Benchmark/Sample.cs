a29a4f7246cdccef36b348445ca539e6ba8589c7
enzo418 <c.enzoaguirre@gmail.com>
2023-08-23 18:52:12 -0300

    Added public benchmark project

---
+++ FisherTournament.Benchmark/Sample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace FisherTournament.Benchmark
+{
+    // Do not modify, create a new file instead.
+    public class /*My*/Benchmarks
+    {
+        [Benchmark]
+        public void Scenario1()
+        {
+            // Implement your benchmark here
+        }
+
+        [Benchmark]
+        public void Scenario2()
+        {
+            // Implement your benchmark here
+        }
+    }
+}
