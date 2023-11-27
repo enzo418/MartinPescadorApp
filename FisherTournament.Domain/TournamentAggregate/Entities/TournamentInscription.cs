d9a287bb02be4c103ccd21b8665076378c73ce67
uenzoaguirre <89944206+uenzoaguirre@users.noreply.github.com>
2023-11-27 05:17:38 -0300

    Added inscription pages

--- FisherTournament.Domain/TournamentAggregate/Entities/TournamentInscription.cs
+++ FisherTournament.Domain/TournamentAggregate/Entities/TournamentInscription.cs
@@ -5,38 +5,38 @@ namespace FisherTournament.Domain.TournamentAggregate.Entities;
 
 public sealed class TournamentInscription : Entity<int>
 {
-    private TournamentInscription(
-        TournamentId tournamentId,
-        FisherId fisherId,
-        CategoryId categoryId,
-        int number)
-        : base()
-    {
-        TournamentId = tournamentId;
-        FisherId = fisherId;
-        CategoryId = categoryId;
-        Number = number;
-    }
+	private TournamentInscription(
+		TournamentId tournamentId,
+		FisherId fisherId,
+		CategoryId categoryId,
+		int number)
+		: base()
+	{
+		TournamentId = tournamentId;
+		FisherId = fisherId;
+		CategoryId = categoryId;
+		Number = number;
+	}
 
-    public TournamentId TournamentId { get; private set; }
-    public FisherId FisherId { get; private set; }
+	public TournamentId TournamentId { get; private set; }
+	public FisherId FisherId { get; private set; }
 
-    public CategoryId CategoryId { get; private set; }
+	public CategoryId CategoryId { get; private set; }
 
-    public int Number { get; private set; }
+	public int Number { get; private set; }
 
-    public static TournamentInscription Create(
-        TournamentId tournamentId,
-        FisherId fisherId,
-        CategoryId categoryId,
-        int number)
-    {
-        return new TournamentInscription(tournamentId, fisherId, categoryId, number);
-    }
+	public static TournamentInscription Create(
+		TournamentId tournamentId,
+		FisherId fisherId,
+		CategoryId categoryId,
+		int number)
+	{
+		return new TournamentInscription(tournamentId, fisherId, categoryId, number);
+	}
 
 #pragma warning disable CS8618
-    private TournamentInscription()
-    {
-    }
+	private TournamentInscription()
+	{
+	}
 #pragma warning restore CS8618
 }
