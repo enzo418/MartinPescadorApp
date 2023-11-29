e867a99f54574f55b9d827004d460451f54ed9f9
uenzoaguirre <89944206+uenzoaguirre@users.noreply.github.com>
2023-11-29 04:38:52 -0300

    Added Tournaments page

--- FisherTournament.API/Controllers[NOTUSED]/CompetitionController.cs
+++ FisherTournament.API/Controllers[NOTUSED]/CompetitionController.cs
@@ -3,8 +3,6 @@ using FisherTournament.Application.Competitions.Commands.AddScore;
 using FisherTournament.Application.Competitions.Queries.GetLeaderBoard;
 using FisherTournament.Application.Tournaments.Commands.AddCompetitions;
 using FisherTournament.Contracts.Competitions;
-using FisherTournament.Domain.CompetitionAggregate.ValueObjects;
-using FisherTournament.Domain.TournamentAggregate.ValueObjects;
 using MapsterMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -15,51 +13,51 @@ namespace FisherTournament.API.Controllers;
 public class CompetitionController : ApiController
 {
 
-    private readonly ISender _sender;
-    private readonly IMapper _mapper;
+	private readonly ISender _sender;
+	private readonly IMapper _mapper;
 
-    public CompetitionController(ISender sender, IMapper mapper)
-    {
-        _sender = sender;
-        _mapper = mapper;
-    }
+	public CompetitionController(ISender sender, IMapper mapper)
+	{
+		_sender = sender;
+		_mapper = mapper;
+	}
 
-    [HttpPost]
-    public async Task<IActionResult> Create(
-        AddCompetitionsRequest request,
-        string tournamentId)
-    {
-        var command = _mapper.Map<AddCompetitionsCommand>((request, tournamentId));
+	[HttpPost]
+	public async Task<IActionResult> Create(
+		AddCompetitionsRequest request,
+		string tournamentId)
+	{
+		var command = _mapper.Map<AddCompetitionsCommand>((request, tournamentId));
 
-        var response = await _sender.Send(command);
+		var response = await _sender.Send(command);
 
-        return response.Match(
-           value => Ok(_mapper.Map<AddCompetitionsResponse>(value)),
-           errors => Problem(errors)
-       );
-    }
+		return response.Match(
+		   value => Ok(_mapper.Map<AddCompetitionsResponse>(value)),
+		   errors => Problem(errors)
+	   );
+	}
 
-    [HttpPost("{competitionId}/scores")]
-    public async Task<IActionResult> AddScore(
-        AddScoreRequest request,
-        [FromRoute] string competitionId)
-    {
-        var command = _mapper.Map<AddScoreCommand>((request, competitionId));
-        var response = await _sender.Send(command);
-        return response.Match(
-            onValue: _ => Ok(),
-            onError: errors => Problem(errors)
-        );
-    }
+	[HttpPost("{competitionId}/scores")]
+	public async Task<IActionResult> AddScore(
+		AddScoreRequest request,
+		[FromRoute] string competitionId)
+	{
+		var command = _mapper.Map<AddScoreCommand>((request, competitionId));
+		var response = await _sender.Send(command);
+		return response.Match(
+			onValue: _ => Ok(),
+			onError: errors => Problem(errors)
+		);
+	}
 
-    [HttpGet("{competitionId}/Leaderboard")]
-    public async Task<IActionResult> GetLeaderboard(string competitionId)
-    {
-        var response = await _sender.Send(new GetCompetitionLeaderBoardQuery(competitionId));
+	[HttpGet("{competitionId}/Leaderboard")]
+	public async Task<IActionResult> GetLeaderboard(string competitionId)
+	{
+		var response = await _sender.Send(new GetCompetitionLeaderBoardQuery(competitionId));
 
-        return response.Match(
-            onValue: value => Ok(value),
-            onError: errors => Problem(errors)
-        );
-    }
+		return response.Match(
+			onValue: value => Ok(value),
+			onError: errors => Problem(errors)
+		);
+	}
 }
