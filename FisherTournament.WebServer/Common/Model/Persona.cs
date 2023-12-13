c94119fd85b87179f81cc4258fb4601182b3b7aa
enzo418 <52799516+enzo418@users.noreply.github.com>
2023-12-13 03:21:46 -0300

    Added notary page and improved UI

---
+++ FisherTournament.WebServer/Common/Model/Persona.cs
@@ -0,0 +1,4 @@
+﻿namespace FisherTournament.WebServer.Common.Model
+{
+	record struct Persona(string Id, string Name, string DNI);
+}
---
+++ FisherTournament.WebServer/Shared/MainLayout.razor.js
@@ -0,0 +1,8 @@
+﻿export function prefeersDarkMode() {
+    let matched = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (matched)
+        return true;
+    else
+        return false;
+}
