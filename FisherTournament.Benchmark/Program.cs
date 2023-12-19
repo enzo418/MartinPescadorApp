f183a4523e9681e9e3fb93a68dc4412270d5a549
enzo418 <52799516+enzo418@users.noreply.github.com>
2023-12-19 06:07:47 -0300

    fixed typo

--- FisherTournament.Benchmark/Program.cs
+++ FisherTournament.Benchmark/Program.cs
@@ -8,7 +8,7 @@ namespace FisherTournament.Benchmark
         public static void Main(string[] args)
         {
             var config = DefaultConfig.Instance;
-            var summary = BenchmarkRunner.Run<MyBenchmarks>(config, args);
+            var summary = BenchmarkRunner.Run<Benchmarks>(config, args);
 
             // Use this to select benchmarks from the console:
             // var summaries = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
